faae2fcb09ed78473d1b5e04a772505e28456c4c
dharadalal <dharadalalm@gmail.com>
2023-02-02 01:43:06 -0500

    Updated File

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("HELLO DHARA")
+const drawrLeftBtn = document.querySelector('.drawer-left');
+const sideNav = document.querySelector(".drawer-content");
+const closeBtn = document.querySelector(".close-navbtn");
+const drawer= document.querySelector(".drawer");
+
+const navBackdrop = document.querySelector(".drawer-backdrop");
+
+drawrLeftBtn.addEventListener('click',()=>{
+    drawer.classList.remove("hide");
+    sideNav.classList.add("show");
+})
+
+navBackdrop.addEventListener('click', closeDrawer);
+
+closeBtn.addEventListener('click', closeDrawer);
+
+function closeDrawer () {
+    sideNav.classList.remove("show");
+    drawer.classList.add("hide");
+}
